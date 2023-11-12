bf7d10011f5e947cdc24d67c3710880bd91d98ec
Derry Renaldy <derryrenaldy2000@gmail.com>
2023-11-12 15:20:29 +0700

    add responsive to expertise 3d modeling

--- PW-Frontend/src/components/hero/Hero.tsx
+++ PW-Frontend/src/components/hero/Hero.tsx
@@ -51,9 +51,9 @@ const Hero = () => {
   return (
     <section ref={containerRef} className="relative">
       {/* <div className="absolute top-0 bottom-0 left-0 right-0" ></div> */}
-      <div>
+      {/* <div>
         <RockModel />
-      </div>
+      </div> */}
       <div className="h-[100svh] flex justify-center items-center z-[100]">
         <div className="z-[100] pointer-events-none text-white font-nunito font-bold text-center flex flex-col gap-y-2">
           {/* <CVWebsite /> */}
