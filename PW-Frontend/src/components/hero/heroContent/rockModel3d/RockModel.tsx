a8fd1bf0f60a4e37dee93afdf3c864225843770a
Derry Renaldy <derryrenaldy2000@gmail.com>
2023-11-13 14:36:26 +0700

    add adactive rendering

--- PW-Frontend/src/components/hero/heroContent/rockModel3d/RockModel.tsx
+++ PW-Frontend/src/components/hero/heroContent/rockModel3d/RockModel.tsx
@@ -1,12 +1,12 @@
 import { Canvas } from "@react-three/fiber";
 import Rock from "./Rock";
 import { Suspense, useState } from "react";
-import { Loader, PerformanceMonitor } from "@react-three/drei";
+import { Bvh, Loader, PerformanceMonitor } from "@react-three/drei";
 // import { Stats } from "@react-three/drei";
 // import { Perf } from "r3f-perf";
 
 const RockModel = () => {
-  const [dpr, setDpr] = useState(1.5);
+  const [dpr, setDpr] = useState(0.6);
   return (
     <div className="h-full w-full z-[100] absolute pointer-events-none">
       <Canvas
@@ -21,12 +21,14 @@ const RockModel = () => {
         {/* <Perf /> */}
         {/* <Stats /> */}
         <PerformanceMonitor
-          onIncline={() => setDpr(2)}
-          onDecline={() => setDpr(1)}
+          onIncline={() => setDpr(1)}
+          onDecline={() => setDpr(0.4)}
         />
-        <Suspense fallback={null}>
-          <Rock />
-        </Suspense>
+        <Bvh firstHitOnly>
+          <Suspense fallback={null}>
+            <Rock />
+          </Suspense>
+        </Bvh>
       </Canvas>
       <Loader />
     </div>
