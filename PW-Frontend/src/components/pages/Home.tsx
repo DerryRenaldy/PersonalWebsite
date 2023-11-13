ab7c9d3477871c436ade46a579ff08379ccce3ce
Derry Renaldy <derryrenaldy2000@gmail.com>
2023-11-13 17:36:32 +0700

    remove suspense from the content component

--- PW-Frontend/src/components/pages/Home.tsx
+++ PW-Frontend/src/components/pages/Home.tsx
@@ -1,15 +1,15 @@
-// import Content from "components/content/Content";
+import Content from "components/content/Content";
 import Hero from "components/hero/Hero";
 import Navbar from "components/navbar/Navbar";
 import CustomScrollbar from "components/scrollbar/CustomScrollbar";
 import { SectionContext } from "./Context";
-import { Suspense, lazy, useState } from "react";
+import { useState } from "react";
 import clsx from "clsx";
-import Image from "@assets/images";
+// import Image from "@assets/images";
 
-const Content = lazy(() =>
-  wait(200).then(() => import("components/content/Content"))
-);
+// const Content = lazy(() =>
+//   wait(200).then(() => import("components/content/Content"))
+// );
 
 const Home = () => {
   const [section, setSection] = useState<string | null>("Home");
@@ -25,16 +25,16 @@ const Home = () => {
       >
         <Navbar />
         <Hero />
-        <Suspense
+        <Content />
+        {/* <Suspense
           fallback={
             <div className="text-white font-rubik text-center h-screen flex flex-col justify-center items-center text-[20px] gap-y-4 font-bold">
               <img src={Image.loadingCircle} className="w-10" />
               <p>Loading Assets...</p>
             </div>
           }
-        >
-          <Content />
-        </Suspense>
+        > */}
+        {/* </Suspense> */}
       </SectionContext.Provider>
     </>
   );
