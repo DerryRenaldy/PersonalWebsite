b5c232c0cd6c9ab79f7ef489beeec5c56de636ac
Derry Renaldy <derryrenaldy2000@gmail.com>
2023-11-13 17:25:05 +0700

    add React.memo in video card

--- PW-Frontend/src/App.tsx
+++ PW-Frontend/src/App.tsx
@@ -1,13 +1,25 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "components/pages/Home";
+import Image from "@assets/images";
+
+const Home = lazy(() => import("components/pages/Home.tsx"));
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Suspense
+      fallback={
+        <div className="text-white font-rubik text-center h-screen flex flex-col justify-center items-center text-[20px] gap-y-4 font-bold">
+          <img src={Image.loadingCircle} className="w-10" />
+          <p>Loading Assets...</p>
+        </div>
+      }
+    >
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
