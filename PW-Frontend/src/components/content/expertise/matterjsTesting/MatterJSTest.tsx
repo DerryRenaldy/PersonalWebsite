51cb38f28a5fe181fd1db7241d5714a46400d4e3
Derry Renaldy <derryrenaldy2000@gmail.com>
2023-11-14 17:41:28 +0700

    add Matter js component

--- PW-Frontend/src/components/content/expertise/matterjsTesting/MatterJSTest.tsx
+++ PW-Frontend/src/components/content/expertise/matterjsTesting/MatterJSTest.tsx
@@ -1,9 +1,10 @@
 import Image from "@assets/images";
 import clsx from "clsx";
 import Matter from "matter-js";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 export const MatterJSTest = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
   const matterRef = useRef<HTMLCanvasElement>(null!);
   const containerRef = useRef<HTMLDivElement>(null!);
 
@@ -20,6 +21,9 @@ export const MatterJSTest = () => {
   let rightWall: Matter.Body;
   let topWall: Matter.Body;
   let ground: Matter.Body;
+  let middleBox: Matter.Body;
+  let render: Matter.Render;
+  let mouseConstraint: Matter.MouseConstraint;
 
   const setWalls = () => {
     leftWall = Bodies.rectangle(
@@ -41,7 +45,7 @@ export const MatterJSTest = () => {
     topWall = Bodies.rectangle(
       matterRef.current.clientWidth / 2,
       0 - THICCNESS / 2,
-      matterRef.current.clientWidth,
+      2000,
       THICCNESS,
       { isStatic: true }
     );
@@ -49,14 +53,27 @@ export const MatterJSTest = () => {
     ground = Bodies.rectangle(
       matterRef.current.clientWidth / 2,
       matterRef.current.clientHeight + THICCNESS / 2,
-      matterRef.current.clientWidth,
+      20000,
       THICCNESS,
       { isStatic: true }
     );
   };
 
+  // useEffect(() => {
+  //   console.log("Is Mobile : " + isMobile);
+  // }, [isMobile]);
+
   useEffect(() => {
-    // Check if matterRef is not null before creating the renderer
+    if (window.innerWidth < 768) {
+      console.log("Mobile");
+      setIsMobile(true);
+    } else {
+      console.log("not Mobile");
+      setIsMobile(false);
+    }
+  }, []);
+
+  useEffect(() => {
     if (!matterRef.current && !containerRef.current) {
       return;
     }
@@ -65,20 +82,21 @@ export const MatterJSTest = () => {
     let engine = Engine.create({ gravity: { y: 0.05, x: 0 } });
 
     // create a renderer
-    let render = Render.create({
+    render = Render.create({
       engine: engine,
       canvas: matterRef.current,
       options: {
         background: "transparent",
         wireframes: false,
-        width: 388,
-        height: 476,
+        // width: 388,
+        width: window.innerWidth / 1.1,
+        height: isMobile ? 450 : 476,
         showAngleIndicator: false,
       },
     });
 
     let mouse = Mouse.create(render.canvas);
-    let mouseConstraint = MouseConstraint.create(engine, {
+    mouseConstraint = MouseConstraint.create(engine, {
       mouse: mouse,
       constraint: {
         stiffness: 0.2,
@@ -90,136 +108,193 @@ export const MatterJSTest = () => {
 
     setWalls();
 
-    // create two boxes and a ground
-    let react = Bodies.circle(0, 0, 45, {
-      friction: 0.3,
-      frictionAir: 0.00001,
-      restitution: 0.8,
-      render: {
-        sprite: {
-          texture: Image.reactLogo,
-          xScale: 0.5,
-          yScale: 0.5,
+    // SET CONTENT
+    let react = Bodies.circle(
+      matterRef.current.clientWidth / 2,
+      0,
+      isMobile ? 45 : 50,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          sprite: {
+            texture: Image.reactLogo,
+            xScale: isMobile ? 0.5 : 0.6,
+            yScale: isMobile ? 0.5 : 0.6,
+          },
         },
-      },
-    });
+      }
+    );
 
-    let docker = Bodies.circle(100, 0, 55, {
-      friction: 0.3,
-      frictionAir: 0.00001,
-      restitution: 0.8,
-      render: {
-        sprite: {
-          texture: Image.dockerLogo,
-          xScale: 0.25,
-          yScale: 0.25,
+    let docker = Bodies.circle(
+      matterRef.current.clientWidth / 3,
+      0,
+      isMobile ? 45 : 80,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          sprite: {
+            texture: Image.dockerLogo,
+            xScale: isMobile ? 0.25 : 0.4,
+            yScale: isMobile ? 0.25 : 0.4,
+          },
         },
-      },
-    });
+      }
+    );
 
-    let golang = Bodies.circle(200, 10, 50, {
-      friction: 0.3,
-      frictionAir: 0.00001,
-      restitution: 0.8,
-      render: {
-        sprite: {
-          texture: Image.golangLogo,
-          xScale: 0.25,
-          yScale: 0.25,
+    let golang = Bodies.rectangle(
+      matterRef.current.clientWidth / 1.2,
+      10,
+      isMobile ? 50 : 80,
+      isMobile ? 100 : 180,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          sprite: {
+            texture: Image.golangLogo,
+            xScale: isMobile ? 0.25 : 0.4,
+            yScale: isMobile ? 0.25 : 0.4,
+          },
         },
-      },
-    });
+      }
+    );
 
-    let js = Bodies.rectangle(100, 20, 55, 55, {
-      friction: 0.3,
-      frictionAir: 0.00001,
-      restitution: 0.8,
-      render: {
-        sprite: {
-          texture: Image.jsLogo,
-          xScale: 0.25,
-          yScale: 0.25,
+    let js = Bodies.rectangle(
+      matterRef.current.clientWidth / 3.5,
+      20,
+      isMobile ? 50 : 90,
+      isMobile ? 50 : 90,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          sprite: {
+            texture: Image.jsLogo,
+            xScale: isMobile ? 0.25 : 0.4,
+            yScale: isMobile ? 0.25 : 0.4,
+          },
         },
-      },
-    });
+      }
+    );
 
-    let ts = Bodies.rectangle(70, 100, 55, 55, {
-      friction: 0.3,
-      frictionAir: 0.00001,
-      restitution: 0.8,
-      render: {
-        sprite: {
-          texture: Image.tsLogo,
-          xScale: 0.25,
-          yScale: 0.25,
+    let ts = Bodies.rectangle(
+      matterRef.current.clientWidth / 3.1,
+      100,
+      isMobile ? 50 : 90,
+      isMobile ? 50 : 90,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          sprite: {
+            texture: Image.tsLogo,
+            xScale: isMobile ? 0.25 : 0.4,
+            yScale: isMobile ? 0.25 : 0.4,
+          },
         },
-      },
-    });
+      }
+    );
 
-    let motion = Bodies.circle(80, 50, 40, {
-      friction: 0.3,
-      frictionAir: 0.00001,
-      restitution: 0.8,
-      render: {
-        sprite: {
-          texture: Image.motionLogo,
-          xScale: 0.25,
-          yScale: 0.25,
+    let motion = Bodies.circle(
+      matterRef.current.clientWidth / 3.2,
+      50,
+      isMobile ? 40 : 60,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          sprite: {
+            texture: Image.motionLogo,
+            xScale: isMobile ? 0.25 : 0.4,
+            yScale: isMobile ? 0.25 : 0.4,
+          },
         },
-      },
-    });
+      }
+    );
 
-    let mysql = Bodies.circle(100, 300, 55, {
-      friction: 0.3,
-      frictionAir: 0.00001,
-      restitution: 0.8,
-      render: {
-        sprite: {
-          texture: Image.mysqlLogo,
-          xScale: 0.25,
-          yScale: 0.25,
+    let mysql = Bodies.rectangle(
+      matterRef.current.clientWidth / 2.4,
+      300,
+      isMobile ? 80 : 160,
+      isMobile ? 60 : 120,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          sprite: {
+            texture: Image.mysqlLogo,
+            xScale: isMobile ? 0.25 : 0.5,
+            yScale: isMobile ? 0.25 : 0.5,
+          },
         },
-      },
-    });
+      }
+    );
 
-    let redis = Bodies.rectangle(90, 0, 150, 40, {
-      friction: 0.3,
-      frictionAir: 0.00001,
-      restitution: 0.8,
-      render: {
-        sprite: {
-          texture: Image.redisLogo,
-          xScale: 0.25,
-          yScale: 0.25,
+    let redis = Bodies.rectangle(
+      matterRef.current.clientWidth / 1.5,
+      0,
+      isMobile ? 160 : 200,
+      isMobile ? 60 : 80,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          sprite: {
+            texture: Image.redisLogo,
+            xScale: isMobile ? 0.25 : 0.3,
+            yScale: isMobile ? 0.25 : 0.3,
+          },
         },
-      },
-    });
+      }
+    );
 
-    let tailwind = Bodies.rectangle(170, 0, 200, 55, {
-      friction: 0.3,
-      frictionAir: 0.00001,
-      restitution: 0.8,
-      render: {
-        sprite: {
-          texture: Image.tailwindLogo,
-          xScale: 0.25,
-          yScale: 0.25,
+    let tailwind = Bodies.rectangle(
+      matterRef.current.clientWidth / 1.2,
+      0,
+      isMobile ? 200 : 280,
+      isMobile ? 55 : 70,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          sprite: {
+            texture: Image.tailwindLogo,
+            xScale: isMobile ? 0.25 : 0.35,
+            yScale: isMobile ? 0.25 : 0.35,
+          },
         },
-      },
-    });
+      }
+    );
 
-    let unity = Bodies.circle(0, 0, 55, {
-      friction: 0.3,
-      frictionAir: 0.00001,
-      restitution: 0.8,
-      render: {
-        sprite: {
-          texture: Image.unityLogo,
-          xScale: 0.25,
-          yScale: 0.25,
+    let unity = Bodies.rectangle(
+      matterRef.current.clientWidth / 6,
+      0,
+      isMobile ? 140 : 180,
+      isMobile ? 30 : 65,
+      {
+        friction: 0.3,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+          sprite: {
+            texture: Image.unityLogo,
+            xScale: isMobile ? 0.25 : 0.35,
+            yScale: isMobile ? 0.25 : 0.35,
+          },
         },
-      },
-    });
+      }
+    );
 
     let cornerBox = Bodies.rectangle(
       0,
@@ -233,6 +308,50 @@ export const MatterJSTest = () => {
         },
       }
     );
+
+    if (!isMobile) {
+      middleBox = Bodies.rectangle(
+        matterRef.current.clientWidth / 2,
+        matterRef.current.clientHeight / 2,
+        280,
+        80,
+        {
+          isStatic: true,
+          render: {
+            fillStyle: "transparent",
+          },
+        }
+      );
+      Composite.add(engine.world, middleBox);
+
+      const colors = [
+        "#F8F8FF",
+        "#CAC9CD",
+        "#9B9A9C",
+        "#6D6A6A",
+        "#3E3B39",
+        "#100C07",
+      ];
+
+      const getRandomColor = () => {
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        return colors[randomIndex];
+      };
+
+      for (let i = 0; i < 400; i++) {
+        let circle = Bodies.circle(
+          Math.random() * (matterRef.current.clientWidth - 10) + 10,
+          i,
+          12,
+          {
+            render: {
+              fillStyle: getRandomColor(),
+            },
+          }
+        );
+        Composite.add(engine.world, circle);
+      }
+    }
 
     // add all of the bodies to the world
     Composite.add(engine.world, [
@@ -254,6 +373,64 @@ export const MatterJSTest = () => {
       mouseConstraint,
     ]);
 
+    // run the renderer
+    Render.run(render);
+
+    // create runner
+    var runner = Runner.create();
+
+    // run the engine
+    Runner.run(runner, engine);
+
+    // Cleanup function
+    return () => {
+      Render.stop(render);
+      Runner.stop(runner);
+    };
+  }, [isMobile]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+
+      render.canvas.width = window.innerWidth / 1.1;
+      render.canvas.height = isMobile ? 450 : 476;
+
+      Matter.Body.setPosition(
+        ground,
+        Matter.Vector.create(
+          matterRef.current.clientWidth / 2,
+          matterRef.current.clientHeight + THICCNESS / 2
+        )
+      );
+
+      Matter.Body.setPosition(
+        leftWall,
+        Matter.Vector.create(
+          0 - THICCNESS / 2,
+          matterRef.current.clientHeight / 2
+        )
+      );
+      Matter.Body.setPosition(
+        rightWall,
+        Matter.Vector.create(
+          matterRef.current.clientWidth + THICCNESS / 2,
+          matterRef.current.clientHeight / 2
+        )
+      );
+      Matter.Body.setPosition(
+        middleBox,
+        Matter.Vector.create(
+          matterRef.current.clientWidth / 2,
+          matterRef.current.clientHeight / 2
+        )
+      );
+    };
+
     mouseConstraint.mouse.element.removeEventListener(
       "mousewheel",
       // @ts-ignore
@@ -265,60 +442,39 @@ export const MatterJSTest = () => {
       mouseConstraint.mouse.mousewheel
     );
 
-    // run the renderer
-    Render.run(render);
-
-    // create runner
-    var runner = Runner.create();
-
-    // run the engine
-    Runner.run(runner, engine);
-
-    const handleResize = () => {
-      // render.canvas.width = window.innerWidth / 1.1;
-      // render.canvas.height = window.innerHeight / 1.1;
-      console.log("Resized");
-
-      Matter.Body.setPosition(
-        ground,
-        Matter.Vector.create(
-          matterRef.current.clientWidth / 2,
-          matterRef.current.clientHeight + THICCNESS / 2
-        )
-      );
-    };
-
     window.addEventListener("resize", handleResize);
 
-    // Cleanup function
     return () => {
-      Render.stop(render);
-      Runner.stop(runner);
       window.removeEventListener("resize", handleResize);
     };
-  }, []); // Empty dependency array means this effect runs once after initial render
+  }, [isMobile]);
+
   return (
     <>
       <div
         ref={containerRef}
-        className={clsx("flex justify-center items-center ", "md:py-[300px]")}
+        className={clsx(
+          "flex justify-center items-center",
+          "md:py-[100px]",
+          "pt-20"
+        )}
       >
         <div
           className={clsx(
-            "p-4 flex flex-col items-center justify-center gap-x-10",
+            "relative p-4 flex flex-col items-center justify-center gap-x-10",
             "md:flex-row",
             "flex-col"
           )}
         >
           <div
             className={clsx(
-              "relative flex border border-[#454444] rounded-xl h-[476px] overflow-hidden filter saturate-50",
-              "md:w-[388px]",
-              "w-[300px]"
+              "relative flex border border-[#454444] rounded-xl h-[476px] overflow-hidden",
+              "w-[calc(100svw/1.1)]",
+              isMobile ? "" : "filter"
             )}
           >
             {/* TEXT */}
-            <div className="absolute left-0 bottom-0 w-[150px] h-[100px] p-4 justify-center flex flex-col z-[100]">
+            <div className="absolute left-0 bottom-0 w-[150px] h-[100px] select-none p-4 justify-center flex flex-col z-[100]">
               <p className="text-white font-rubik font-bold text-lg">
                 My Stack
               </p>
@@ -336,8 +492,14 @@ export const MatterJSTest = () => {
               className="absolute top-0 rounded-xl left-1/2 -translate-x-[50%]"
             />
           </div>
-          <div className="text-white h-full p-6 font-rubik border">
-            Web Development
+          <div
+            className={`absolute text-white p-4 pointer-events-none font-semibold select-none font-rubik  ${
+              isMobile
+                ? "bg-[#8d8d8d]/10 flex justify-center items-center rounded-xl backdrop-blur-sm text-[20px] top-4 bottom-4 left-4 right-4"
+                : "text-[30px] h-fit"
+            }`}
+          >
+            <p>Web Development</p>
           </div>
         </div>
       </div>
