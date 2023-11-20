ed7de0f85db756742a1ff913b496a6433e821fe1
Derry Renaldy <derryrenaldy2000@gmail.com>
2023-11-20 15:35:57 +0700

    optimizing for mobile version

--- PW-Frontend/src/App.tsx
+++ PW-Frontend/src/App.tsx
@@ -1,25 +1,26 @@
-import { Suspense, lazy } from "react";
+// import { Suspense, lazy } from "react";
+import Home from "components/pages/Home.tsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Image from "@assets/images";
+// import Image from "@assets/images";
 
-const Home = lazy(() => import("components/pages/Home.tsx"));
+// const Home = lazy(() => import("components/pages/Home.tsx"));
 
 function App() {
   return (
-    <Suspense
-      fallback={
-        <div className="text-white font-rubik text-center h-screen flex flex-col justify-center items-center text-[20px] gap-y-4 font-bold">
-          <img src={Image.loadingCircle} className="w-10" />
-          <p>Loading Assets...</p>
-        </div>
-      }
-    >
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </Suspense>
+    // <Suspense
+    //   fallback={
+    //     <div className="text-white font-rubik text-center h-screen flex flex-col justify-center items-center text-[20px] gap-y-4 font-bold">
+    //       <img src={Image.loadingCircle} className="w-10" />
+    //       <p>Loading Assets...</p>
+    //     </div>
+    //   }
+    // >
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+    // </Suspense>
   );
 }
 
