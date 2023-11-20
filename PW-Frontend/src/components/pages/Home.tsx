53701a0e5ccc8c956de50cf2ebdb17ec410bd1c7
Derry Renaldy <derryrenaldy2000@gmail.com>
2023-11-20 15:48:08 +0700

    add fake loading and split the code

--- PW-Frontend/src/components/pages/Home.tsx
+++ PW-Frontend/src/components/pages/Home.tsx
@@ -1,42 +1,58 @@
-import Content from "components/content/Content";
-import Hero from "components/hero/Hero";
-import Navbar from "components/navbar/Navbar";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import CustomScrollbar from "components/scrollbar/CustomScrollbar";
 import { SectionContext } from "./Context";
-import { useState } from "react";
+import Image from "@assets/images";
 import clsx from "clsx";
-// import CustomName from "components/customName/CustomName";
-// import Image from "@assets/images";
 
-// const Content = lazy(() =>
-//   wait(200).then(() => import("components/content/Content"))
-// );
+const Navbar = lazy(() => import("components/navbar/Navbar"));
+const Hero = lazy(() => import("components/hero/Hero"));
+const Content = lazy(() => import("components/content/Content"));
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
   const [section, setSection] = useState<string | null>("Home");
   const [isScrolledCtx, setIsScrolledCtx] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fakeLoadingTimeout = setTimeout(() => {
+      // Simulate loading completion
+      setLoading(false);
+    }, 1000); // Adjust the duration as needed
+
+    return () => {
+      // Clear the timeout if the component is unmounted
+      clearTimeout(fakeLoadingTimeout);
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="text-white font-rubik text-center h-screen flex flex-col justify-center items-center text-[20px] gap-y-4 font-bold">
+        <img src={Image.loadingCircle} className="w-10" alt="Loading" />
+        <p>Loading Assets...</p>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="noise" />
       <div className={clsx("md:block", "hidden")}>
         <CustomScrollbar />
       </div>
+
       <SectionContext.Provider
         value={{ section, setSection, isScrolledCtx, setIsScrolledCtx }}
       >
-        <Navbar />
-        <Hero />
-        <Content />
-        {/* <CustomName /> */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Navbar />
+          <Hero />
+          <Content />
+          {/* <CustomName /> */}
+        </Suspense>
       </SectionContext.Provider>
     </>
   );
 };
-
-// function wait(time: number) {
-//   return new Promise((resolve) => {
-//     setTimeout(resolve, time);
-//   });
-// }
 
 export default Home;
